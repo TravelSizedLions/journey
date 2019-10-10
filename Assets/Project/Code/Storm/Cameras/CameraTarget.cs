5a6a495fa176aedcb0f028199b3cd1c21539bbd3
Jeff Hilton <jeffhilton.code@gmail.com>
2019-10-10 13:12:28 -0500

    Update CameraTarget.cs

--- Assets/Project/Code/Storm/Cameras/CameraTarget.cs
+++ Assets/Project/Code/Storm/Cameras/CameraTarget.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace Storm.Cameras {
-
+    
+    /// <summary>
+    /// A virtual camera used to frame sections of a level 
+    /// </summary>
     public class CameraTarget : MonoBehaviour {
 
+        [Tooltip("")]
+        /// <summary>
+        /// 
+        /// </summary>
         public bool freezeX;
 
+        [Tooltip("")]
+        /// <summary>
+        /// 
+        /// </summary>
         public bool freezeY;
 
         private Camera cameraSettings;
         private static TargettingCamera cam;
 
         // Start is called before the first frame update
+        /// <summary>
+        ///
+        /// </summary>
         void Start(){
             cameraSettings = GetComponent<Camera>();
             if (cameraSettings == null) {
@@ -26,6 +39,9 @@ namespace Storm.Cameras {
             }
         }
 
+        /// <summary>
+        ///
+        /// </summary>
         public void OnTriggerEnter2D(Collider2D col) {
             if (col.gameObject.CompareTag("Player")) {
                 cam.SetTarget(cameraSettings);
@@ -34,6 +50,9 @@ namespace Storm.Cameras {
             }
         }
 
+        /// <summary>
+        ///
+        /// </summary>
         public void OnTriggerStay2D(Collider2D col) {
             if (col.gameObject.CompareTag("Player")) {
                 if (cam.target.transform.position != cameraSettings.transform.position) {
@@ -45,6 +64,9 @@ namespace Storm.Cameras {
             }
         }
 
+        /// <summary>
+        ///
+        /// </summary>
         public void OnTriggerExit2D(Collider2D col) {
             if (col.gameObject.CompareTag("Player")) {
                 // In case 2 zones overlap
