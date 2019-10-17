1424140ae32fab885bb49f45c586be3752905f07
Jeff Hilton <jeffhilton.code@gmail.com>
2019-10-17 00:49:46 -0500

    Organizing Targetting Camera props
    Organizing the Targetting Camera's editor properties.

--- Assets/Project/Code/Storm/Attributes/ReadOnlyAttribute.cs
+++ Assets/Project/Code/Storm/Attributes/ReadOnlyAttribute.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Storm.Attributes {
+    /// <summary>
+    /// This attribute stub makes it possible to mark a public variable
+    /// as visible to the unity editor, but not editable.
+    /// </summary>
     public class ReadOnlyAttribute : PropertyAttribute {}
 }
--- Assets/Project/Code/Storm/Cameras/TargettingCamera.cs
+++ Assets/Project/Code/Storm/Cameras/TargettingCamera.cs
@@ -8,27 +8,114 @@ using Storm.TransitionSystem;
 namespace Storm.Cameras {
     public class TargettingCamera : MonoBehaviour
     {
+        #region Offset Parameters
+        [Header("Offsets", order=0)]
+        [Space(5, order=1)]
 
+        /// <summary>
+        /// How much the camera should be offset from the player vertically.
+        /// </summary>
+        [Tooltip("How much the camera should be offset from the player vertically.")]
+        public float verticalOffset;
+
+        /// <summary>
+        /// How much the camera should be offset from the player horizontally.
+        /// </summary>
+        [Tooltip("How much the camera should be offset from the player horizontally.")]
+        public float horizontalOffset;
+
+        /// <summary>
+        /// The calculated offset vector when centering on a target.
+        /// </summary>
+        private Vector3 centeredOffset;
+
+        /// <summary>
+        /// The calculated offset vector when offsetting to the left of a target.
+        /// </summary>
+        private Vector3 leftOffset;
+
+        /// <summary>
+        /// The calculated offset vector when offsetting to the right of a target.
+        /// </summary>
+        private Vector3 rightOffset;
+
+        [Space(15, order=2)]
+        #endregion
+
+
+        #region Targetting Speed Parameters
+        [Header("Targetting Speed", order=3)]
+        [Space(5, order=4)]
+
+        /// <summary>
+        /// How quickly to pan the camera to a vCam target.
+        /// </summary>
+        [Tooltip("How quickly to pan the camera to a vCam target. 0 - No panning, 1 - Instantaneous")]
+        [Range(0,1)]
+        public float vCamPanSpeed;
+
+        /// <summary>
+        /// How quickly to pan the camera to the player if their the target.
+        /// </summary>
+        [Tooltip("How quickly to pan the camera to the player if their the target. 0 - No panning, 1 - Instantaneous")]
+        [Range(0,1)]
+        public float playerPanSpeed;
+
+        /// <summary>
+        /// How quickly to zoom the camera in and out.
+        /// </summary>
+        [Tooltip("How quickly to zoom the camera in and out. 0 - No panning, 1 - Instantaneous")]
+        [Range(0,1)]
+        public float zoomSpeed;
+
+        #endregion
+
+
+        #region Static Variables
+
+        /// <summary>
+        /// The transform that the Camera is actually tracking.
+        /// </summary>
+        public static Transform target;
+
+        /// <summary>
+        /// The name of a virtual camera to snap to at the beginning of a scene, if applicable.
+        /// </summary>
         private static string virtualCameraName;
 
-        public static Transform target;
+        /// <summary>
+        /// A reference to the player
+        /// </summary>
         public static PlayerCharacter player;
-        public float targetSmoothing;
-        public float playerSmoothing;
 
-        public float sizeSmoothing;
-        public Vector3 velocity;
+        /// <summary>
+        /// Whether or not the camera should center on the target instead of offsetting.
+        /// </summary>
+        private static bool isCentered;
 
-        public Vector3 targetOffset;
-        public Vector3 leftOffset;
-        public Vector3 rightOffset;
-        public static bool isCentered;
-
+        /// <summary>
+        /// The target's camera settings, such as orthographic size (zoom).
+        /// </summary>
         private static Camera targetSettings;
 
+        /// <summary>
+        /// The camera settings that the targetting camera defaults to if not given a virtual camera to target.
+        /// </summary>
         private static Camera defaultSettings;
 
+        /// <summary>
+        /// A reference parameter used by SmoothDamp in the FixedUpdate function
+        /// </summary>
+        private Vector3 velocity;
+
+        #endregion
+
         void Start() {
+            centeredOffset = new Vector3(0, 0, -10);
+            leftOffset = new Vector3(-horizontalOffset, verticalOffset, -10);
+            rightOffset = new Vector3(-horizontalOffset, verticalOffset, -10);
+
+
             Debug.Log("Camera Startup");
             defaultSettings = GetComponent<Camera>();
             if (player == null) {
@@ -67,11 +154,11 @@ namespace Storm.Cameras {
 
             
             float futureSize = targetSettings.orthographicSize;
-            float smoothing = (target == player.transform) ? playerSmoothing : targetSmoothing;
+            float smoothing = (target == player.transform) ? playerPanSpeed : vCamPanSpeed;
             Vector3 futurePos = getFuturePosition();
             
             // interpolate camera zoom
-            Camera.main.orthographicSize = interpolate(Camera.main.orthographicSize, targetSettings.orthographicSize, sizeSmoothing);
+            Camera.main.orthographicSize = interpolate(Camera.main.orthographicSize, targetSettings.orthographicSize, zoomSpeed);
             
             // interpolate camera position
             transform.position = Vector3.SmoothDamp(transform.position, futurePos, ref velocity, smoothing);
@@ -87,7 +174,7 @@ namespace Storm.Cameras {
 
                 // choose appropriate camera offset.
                 if (isCentered) {
-                    pos += targetOffset;
+                    pos += centeredOffset;
                 } else if (player.normalMovement.isFacingRight) {
                     pos += rightOffset;
                 } else if (!player.normalMovement.isFacingRight) {
@@ -96,7 +183,7 @@ namespace Storm.Cameras {
                 
             // moving to a predefined location
             } else {
-                pos = target.transform.position + targetOffset;
+                pos = target.transform.position + centeredOffset;
             }
 
             return pos;
@@ -109,11 +196,11 @@ namespace Storm.Cameras {
 
 
         public void SetPlayerSmoothing(float smoothing) {
-            playerSmoothing = smoothing;
+            playerPanSpeed = smoothing;
         }
 
         public void SetTargetSmoothing(float smoothing) {
-            targetSmoothing = smoothing;
+            vCamPanSpeed = smoothing;
         }
 
         public static void SetTarget(string vcamName) {
@@ -143,7 +230,7 @@ namespace Storm.Cameras {
                     transform.position += leftOffset;
                 }
             } else if (isCentered) {
-                transform.position += targetOffset;
+                transform.position += centeredOffset;
             }
         }
 
