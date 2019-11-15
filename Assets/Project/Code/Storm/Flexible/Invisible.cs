be935b2dc920dac137881ea557e1a66b1517ffd0
Jeff Hilton <jeffhilton.code@gmail.com>
2019-11-14 22:55:22 -0600

    Invisible should now cover child elements & UI Images

--- Assets/Project/Code/Storm/Flexible/Invisible.cs
+++ Assets/Project/Code/Storm/Flexible/Invisible.cs
@@ -1,12 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Storm.Flexible {
 
     public class Invisible : MonoBehaviour {
-        void Start() {
-            GetComponent<SpriteRenderer>().enabled = false;
+        void Awake() {
+            var sprite = GetComponent<SpriteRenderer>();
+            if (sprite != null) {
+                sprite.enabled = false;
+            }
+
+            foreach (var child in GetComponentsInChildren<SpriteRenderer>(true)) {
+                child.enabled = false;
+            }
+
+
+            var image = GetComponent<Image>();
+            if (image != null) {
+                image.enabled = false;
+            }
+
+            foreach (var child in GetComponentsInChildren<Image>(true)) {
+                child.enabled = false;
+            }
         }
     }
 }
