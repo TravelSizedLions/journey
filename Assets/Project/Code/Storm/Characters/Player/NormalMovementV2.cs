efb242b27d1f2ac7fc9a128ef15fc30bc3045444
Jeff Hilton <jeffhilton.code@gmail.com>
2019-12-01 21:31:59 -0600

    Starting on Player movement changes

---
+++ Assets/Project/Code/Storm/Characters/Player/NormalMovementV2.cs
@@ -0,0 +1,67 @@
+using Storm.LevelMechanics.Platforms;
+using UnityEngine;
+
+namespace Storm.Characters.Player {
+    public class NormalMovementV2 : PlayerBehavior {
+
+        public float horizontalSpeed;
+
+        private bool canJump;
+        public float singleJumpHeight;
+
+        private Vector2 singleJumpForce;
+
+        public float doubleJumpHeight;
+
+        private Vector2 doubleJumpForce;
+
+        private bool canDoubleJump;
+
+
+        public void Start() {
+            singleJumpForce = new Vector2(0, singleJumpHeight);
+            doubleJumpForce = new Vector2(0, doubleJumpHeight);
+        }
+
+        public void Update() {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                HandleJump();
+            }
+        }
+
+
+        public void HandleJump() {
+            if (canJump) {
+                rb.velocity += singleJumpForce;
+                canJump = false;
+            } else if (canDoubleJump) {
+                rb.velocity += doubleJumpForce;
+                canDoubleJump = false;
+            }
+        }
+
+
+
+        public void FixedUpdate() {
+            float hAxis = Input.GetAxis("Horizontal");
+
+
+        }
+
+
+        //---------------------------------------------------------------------
+        // Player Behavior API
+        //---------------------------------------------------------------------
+        public override void Activate() {
+
+        }
+
+        public override void Deactivate() {
+
+        }
+
+        //---------------------------------------------------------------------
+        // Collider Logic
+        //---------------------------------------------------------------------
+    }
+}
