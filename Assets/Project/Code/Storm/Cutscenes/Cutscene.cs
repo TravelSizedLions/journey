5aae1beb69ca6c43c14ffffd945b603e9bc1197b
Jeff Hilton <jeffhilton.code@gmail.com>
2019-12-07 11:53:44 -0600

    Reworking Cutscenes

---
+++ Assets/Project/Code/Storm/Cutscenes/Cutscene.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Storm.TransitionSystem;
+
+
+namespace Storm.Cutscenes {
+
+    /// <summary>
+    /// A class for playing a cutscene slideshow to the player.
+    /// </summary>
+    [RequireComponent(typeof(Image))]
+    public class Cutscene : MonoBehaviour
+    {
+        public List<Sprite> Images;
+
+        private Image screen;
+
+        public string NextScene;
+
+        public string NextSpawn;
+
+        private int currentImage;
+
+
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            if (Images == null) {
+                Images = new List<Sprite>();
+            }
+
+            currentImage = 0;
+            
+            screen = GetComponent<Image>();
+
+            if (Images.Count > 0) {
+                screen.sprite = Images[0];
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                // if it's not the last image:
+                //  Go to the next image.
+                if (currentImage != Images.Count-1) {
+                    NextImage();
+                } else {
+                    ChangeScenes();
+                }
+            }
+        }
+
+        public void NextImage() {
+            currentImage++;
+            screen.sprite = Images[currentImage];
+        }
+
+
+        public void ChangeScenes() {
+            TransitionManager.Instance.MakeTransition(NextScene, NextSpawn);
+        }
+    }
+
+}
---
+++ Assets/Project/Code/Storm/Cutscenes/Slide.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Storm.Cutscenes {
+
+    /// <summary>
+    /// Represents a single image in a cutscene.
+    /// </summary>
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(Sprite))]
+    public class Slide : MonoBehaviour {
+        public Sprite image;
+
+        public Animator transitions;
+
+        /// <summary>
+        /// Delegate type.
+        /// </summary>
+        public delegate void SlideDelegate();
+
+        /// <summary>
+        /// List of subscribers to be notified when
+        /// the slide ends.
+        /// </summary>
+        private SlideDelegate subscribers;
+
+
+        // Start is called before the first frame update
+        void Awake() {
+            image = GetComponent<Sprite>();
+            transitions = GetComponent<Animator>();
+        }
+
+        /// <summary>
+        /// Play the slide.
+        /// </summary>
+        /// <returns>The slide image that was played</returns>
+        public Sprite PlaySlide() {
+            transitions.SetTrigger("play");
+            return image;
+        }
+
+        /// <summary>
+        /// End the slide.
+        /// </summary>
+        public void EndSlide() {
+            transitions.SetTrigger("end");
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Subscribe(SlideDelegate sub) {
+            subscribers += sub;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sub"></param>
+        public void Unsubscribe(SlideDelegate sub) {
+            subscribers -= sub;
+        }
+
+        /// <summary>
+        /// Notify subscribers that the slide has ended.
+        /// </summary>
+        public void OnSlideEnded() {
+            subscribers();
+        }
+    }
+}
