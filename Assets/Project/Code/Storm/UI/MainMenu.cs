85cae25fd98148b6b8d2b6c8599e77ff0bdea5e8
Jeff Hilton <jeffhilton.code@gmail.com>
2019-12-07 12:28:26 -0600

    Testing level transitions before main build.

--- Assets/Project/Code/Storm/UI/MainMenu.cs
+++ Assets/Project/Code/Storm/UI/MainMenu.cs
@@ -7,10 +7,30 @@ using Storm.TransitionSystem;
 
 namespace Storm.UI {
     public class MainMenu : MonoBehaviour {
+
+        /// <summary>
+        /// The name of the scene to load.
+        /// </summary>
+        [Tooltip("The name of the scene to load.")]
+        public string SceneName;
+
+        /// <summary>
+        /// The name of the spawn position for the player.
+        /// </summary>
+        [Tooltip("The name of the spawn position for the player.")]
+        public string SpawnName;
+
+
+        /// <summary>
+        /// Start playing the game.
+        /// </summary>
         public void PlayGame() {
-            TransitionManager.Instance.MakeTransition("Cutscene");
+            TransitionManager.Instance.MakeTransition(SceneName, SpawnName);
         }
 
+        /// <summary>
+        /// Quit playing the game.
+        /// </summary>
         public void QuitGame() {
             Debug.Log("Quitting!");
             Application.Quit();
