bde7c84eb18301f33d8f01081afff18abfed850f
Jeff Hilton <jeffhilton.code@gmail.com>
2020-04-12 21:17:53 -0500

    Add documentation to Sound.cs

--- Assets/Project/Code/Storm/AudioSystem/Sound.cs
+++ Assets/Project/Code/Storm/AudioSystem/Sound.cs
@@ -7,26 +7,37 @@ using Storm.Attributes;
 
 namespace Storm.AudioSystem {
 
+    ///<summary>
+    /// An in-game sound effect.
+    ///</summary>
     [Serializable]
     public class Sound  {
 
+        /** The name of the sound. */
         public string Name;
 
+        /** The sound file. */
         public AudioClip Clip;
 
+        /** How loud the sound will play. */
         [Range(0f, 1f)]
         public float Volume = 1f;
 
+        /** Adjusts the fequency of the sound up or down. */
         [Range(0.1f, 3f)]
         public float Pitch = 1f;
 
+        /** How long to wait before playing the sound. */
         [HideInInspector]
         public float Delay = 0;
 
+        /** The source of the sound. Needed by the AudioManager to play the sound. */
         [ReadOnly]
         public AudioSource source;
 
-
+        ///<summary>
+        /// Makes a copy of the sound, minus a source.
+        ///</summary>
         public Sound Copy() {
             Sound copy = new Sound();
             copy.Name = Name;
@@ -38,6 +49,9 @@ namespace Storm.AudioSystem {
             return copy;
         }
 
+        ///<summary>
+        /// Prepare the sound to be played.
+        ///</summary>
         public void Reload(GameObject gameObject) {
             source = gameObject.AddComponent<AudioSource>();
             source.clip = Clip;
