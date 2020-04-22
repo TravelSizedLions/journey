f94e8e66805e0950ebc11677b87aacae50ab549b
Jeff Hilton <jeffhilton.code@gmail.com>
2020-04-21 21:06:10 -0500

    Updating OneWayPlatform to be closer to the Style Checklist.

--- Assets/Project/Code/Storm/LevelMechanics/Platforms/OneWayPlatform.cs
+++ Assets/Project/Code/Storm/LevelMechanics/Platforms/OneWayPlatform.cs
@@ -6,33 +6,67 @@ using UnityEngine;
 
 namespace Storm.LevelMechanics.Platforms {
 
-  /**
-    Platforms that players can hop onto from below.
-    Pressing down lets the player through the platform.
-    
-    Use on a parent object with a composite collider.
-    +----------------------+
-    | +----+ +----+ +----+ |
-    | |    | |    | |    | | 
-    | +----+ +----+ +----+ |
-    +----------------------+
-  */
+  /// <summary>
+  /// Platforms that players can hop onto from below.
+  /// Pressing down lets the player through the platform.
+  /// Use on a parent object with a composite collider.
+  /// +----------------------+
+  /// | +----+ +----+ +----+ |
+  /// | |    | |    | |    | | 
+  /// | +----+ +----+ +----+ |
+  /// +----------------------+
+  /// </summary>
   public class OneWayPlatform : MonoBehaviour {
 
+    #region Variables
+
+    #region Player Variables
+    /// <summary>
+    /// A reference to the player character.
+    /// </summary>
     private static PlayerCharacter player;
 
+    /// <summary>
+    /// A reference to the player's collider.
+    /// </summary>
     private static BoxCollider2D playerCollider;
 
-    /** Platform object > Platform sections*/
-    private BoxCollider2D platformCollider;
-
+    /// <summary>
+    /// Whether or not the player is trying to drop through the platform.
+    /// </summary>
     private bool droppingThrough;
 
+    /// <summary>
+    /// Whether or not the player is touching this platform.
+    /// </summary>
     private bool playerIsTouching;
 
+    #endregion
+
+
+    #region Temporary Collider Disabling Variables
+    /// <summary>
+    /// The platform's collider.
+    /// </summary>
+    private BoxCollider2D platformCollider;
+
+
+    /// <summary>
+    /// A timer to keep the platform's collider disabled for a period.
+    /// </summary>
     private float disableTimer;
 
+    /// <summary>
+    /// How long to disable the platform's collider when the player is trying to drop through.
+    /// </summary>
     public float disabledTime = 0.5f;
+    #endregion
+    #endregion
+
+    #region Unity API
+    //-------------------------------------------------------------------------
+    // Unity API
+    //-------------------------------------------------------------------------
 
     public void Awake() {
       if (player == null) {
@@ -92,5 +126,7 @@ namespace Storm.LevelMechanics.Platforms {
         playerIsTouching = false;
       }
     }
+
+    #endregion
   }
 }
