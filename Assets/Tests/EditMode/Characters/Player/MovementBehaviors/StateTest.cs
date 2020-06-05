f9fb900b9b14f39df02935388de898f9d159fc9b
Jeff Hilton <jeffhilton.code@gmail.com>
2020-06-04 22:33:01 -0500

    Remove empty tests

--- Assets/Tests/EditMode/Characters/Player/MovementBehaviors/StateTest.cs
+++ Assets/Tests/EditMode/Characters/Player/MovementBehaviors/StateTest.cs
@@ -35,21 +35,6 @@ namespace Tests {
 
       state.Inject(player, physics);
     }
-
-    // A Test behaves as an ordinary method
-    [Test]
-    public void StateTestSimplePasses() {
-      // Use the Assert class to test conditions
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator StateTestWithEnumeratorPasses() {
-      // Use the Assert class to test conditions.
-      // Use yield to skip a frame.
-      yield return null;
-    }
   }
 
 }
