ab81c61c14dec5202103dcd4b551b1731f40011e
Jeff Hilton <jeffhilton.code@gmail.com>
2020-10-31 02:11:44 -0500

    Dynamic mouse resizing, UI scaling.

--- Assets/Production/0_Code/Storm/UI/Mouse/MouseIcon.cs
+++ Assets/Production/0_Code/Storm/UI/Mouse/MouseIcon.cs
@@ -2,6 +2,7 @@ using System;
 using Sirenix.OdinInspector;
 using Storm.Characters.Player;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 namespace Storm.UI {
 
@@ -10,13 +11,50 @@ namespace Storm.UI {
   /// </summary>
   [Serializable]
   public class MouseIcon {
+
+
+    #region Constants
+    //-------------------------------------------------------------------------
+    // Constants
+    //-------------------------------------------------------------------------
+
+
+    // The minimum height required to use a 128x128 cursor
+    private static int SIZE_128_BOUNDARY = 2400;
+
+    // The minimum height required to use a 64x64 cursor
+    private static int SIZE_64_BOUNDARY = 1400;
+
+    #endregion
+
+    #region Fields
+    //-------------------------------------------------------------------------
+    // Fields
+    //-------------------------------------------------------------------------
+
+    /// <summary>
+    /// The sprite for the icon (32x32 version).
+    /// </summary>
+    [Tooltip("The sprite for the icon (32x32).")]
+    [PreviewField(32, ObjectFieldAlignment.Center)]
+    [TableColumnWidth(64, false)]
+    public Texture2D Sprite32;
+
     /// <summary>
     /// The sprite for the icon.
     /// </summary>
-    [Tooltip("The sprite for the icon.")]
+    [Tooltip("The sprite for the icon (64x64).")]
     [PreviewField(32, ObjectFieldAlignment.Center)]
-    [TableColumnWidth(40, false)]
-    public Texture2D Sprite;
+    [TableColumnWidth(64, false)]
+    public Texture2D Sprite64;
+
+    /// <summary>
+    /// The sprite for the icon.
+    /// </summary>
+    [Tooltip("The sprite for the icon (128x128).")]
+    [PreviewField(32, ObjectFieldAlignment.Center)]
+    [TableColumnWidth(64, false)]
+    public Texture2D Sprite128;
 
 
     /// <summary>
@@ -25,12 +63,50 @@ namespace Storm.UI {
     [Tooltip("The name of the mouse icon.")]
     public string Name;
 
+    #endregion
+
+    
+    #region Public API
+    //-------------------------------------------------------------------------
+    // Public API
+    //-------------------------------------------------------------------------
+
     /// <summary>
     /// Swap in this mouse icon.
     /// </summary>
     public void Swap() {
-      Cursor.SetCursor(Sprite, new Vector2(16, 16), CursorMode.ForceSoftware);
+      Debug.Log("swapping!");
+      Texture2D icon = GetSprite();
+      Vector2 hotspot = new Vector2(icon.width/2, icon.height/2);
+      Cursor.SetCursor(icon, hotspot, CursorMode.ForceSoftware);
     }
+
+    #endregion
+
+
+    #region Helper Method
+    //-------------------------------------------------------------------------
+    // Helper Method
+    //-------------------------------------------------------------------------
+
+    /// <summary>
+    /// Dynamically resize a mouse icon based on the system's resolution.
+    /// </summary>
+    /// <param name="mouseIcon">The source texture.</param>
+    /// <returns>The resized texture</returns>
+    private Texture2D GetSprite() {
+      int displayHeight = Display.main.systemHeight;
+
+      if (displayHeight < SIZE_64_BOUNDARY) {
+        return Sprite32;
+      } else if (displayHeight < SIZE_128_BOUNDARY) {
+        return Sprite64;
+      } else {
+        return Sprite128;
+      }
+    }
+
+    #endregion
   }
 
 }
