e1318ff0ef6b51fd8952ada222440390fc59fb04
Jeff Hilton <jeffhilton.code@gmail.com>
2020-10-31 05:59:31 -0500

    Decorating. :)

---
+++ Assets/Production/0_Code/Storm/Environment/Parallax.cs
@@ -0,0 +1,57 @@
+﻿using Storm.Cameras;
+using UnityEngine;
+
+namespace Storm.Environment {
+
+  public class Parallax : MonoBehaviour {
+
+    #region Fields
+    //-------------------------------------------------------------------------
+    // Fields
+    //-------------------------------------------------------------------------
+    
+    /// <summary>
+    /// The background object to have be part of the effect.
+    /// </summary>
+    [Tooltip("The background object to have be part of the effect.")]
+    [SerializeField]
+    private Transform background = null;
+
+    /// <summary>
+    /// The scene's camera.
+    /// </summary>
+    [Tooltip("The scene's camera.")]
+    [SerializeField]
+    private TargettingCamera targettingCamera = null;
+    #endregion
+
+    #region Unity API
+    //-------------------------------------------------------------------------
+    // Unity API
+    //-------------------------------------------------------------------------
+
+    private void Start() {
+      if (background == null) {
+        background = transform;
+      }
+
+      if (targettingCamera == null) {
+        targettingCamera = FindObjectOfType<TargettingCamera>();
+      }
+    }
+
+
+    private void Update() {
+      Vector3 pos = new Vector3(
+        targettingCamera.transform.position.x,
+        targettingCamera.transform.position.y,
+        background.position.z
+      );
+      background.position = targettingCamera.transform.position;
+    }
+
+
+    #endregion
+  }
+
+}
