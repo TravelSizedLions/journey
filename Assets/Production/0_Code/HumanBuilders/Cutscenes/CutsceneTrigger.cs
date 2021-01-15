e0eb4bc87931f25cfe68bb8a8c5b65d121e59bca
Jeff Hilton <jeffhilton.code@gmail.com>
2021-01-14 20:55:21 -0600

    Remove InGameCutscene, create CutsceneTrigger.

---
+++ Assets/Production/0_Code/HumanBuilders/Cutscenes/CutsceneTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace HumanBuilders {
+  /// <summary>
+  /// A class that triggers a cutscene automatically on loading a new scene.
+  /// </summary>
+  public class CutsceneTrigger : MonoBehaviour {
+
+    /// <summary>
+    /// The cutscene to trigger.
+    /// </summary>
+    [Tooltip("The cutscene to trigger.")]
+    public AutoGraph Cutscene;
+
+    /// <summary>
+    /// The number of seconds to delay before starting the cutscene.
+    /// </summary>
+    [Tooltip("The number of seconds to delay before starting the cutscene.")]
+    public float Delay;
+
+    private void Awake() {
+      new UnityTask(_Trigger());
+    }
+
+    private IEnumerator _Trigger() {
+      if (Delay > 0) {
+        yield return new WaitForSeconds(Delay);
+      }
+
+      if (Cutscene != null) {
+        DialogManager.StartDialog(Cutscene);
+      }
+    }
+
+  }
+
+}
