b0b6dbab23c14582b149a2de389d00366490330b
Jeff Hilton <jeffhilton.code@gmail.com>
2021-01-14 21:02:17 -0600

    Adding very convenient utility. :)

---
+++ Assets/Production/0_Code/HumanBuilders/Editor/DestroyMissingScripts.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HumanBuilders {
+
+  public static class CleanUpMissingScripts {
+    [MenuItem("GameObject/Remove Missing Scripts", false, 48)]
+    public static void CleanupMissingScripts() {
+
+      for (int sIndex = 0; sIndex < SceneManager.sceneCount; sIndex++) {
+        Scene scene = SceneManager.GetSceneAt(sIndex);
+
+        int sceneTotal = 0;
+
+        GameObject[] rootObjects = scene.GetRootGameObjects();
+        foreach (GameObject gameObject in rootObjects) {
+          sceneTotal += CleanUpGameObject(gameObject);
+        }
+
+        Debug.Log("Removed " + sceneTotal + " missing scripts from scene \"" + scene.name  + ".\"");
+      }
+
+    }
+
+
+    public static int CleanUpGameObject(GameObject gameObject) {
+      int removed = 0;
+      if (gameObject.transform.childCount == 0) {
+        removed = GameObjectUtility.RemoveMonoBehavioursWithMissingScript(gameObject);
+        if (removed > 0) {
+          EditorUtility.SetDirty(gameObject);
+        }
+      } else {
+        foreach (Transform child in gameObject.transform) {
+          removed += CleanUpGameObject(child.gameObject);
+        }
+      }
+
+      return removed;
+    }
+  }
+}
