92f869a184d9036bf85b7127886062e072ea6dce
Jeff Hilton <jeffhilton.code@gmail.com>
2021-05-10 23:22:26 -0600

    Printing XML test result.

--- Assets/Production/0_Code/HumanBuilders/Editor/CLI/TestCommand.cs
+++ Assets/Production/0_Code/HumanBuilders/Editor/CLI/TestCommand.cs
@@ -1,6 +1,8 @@
 #if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace HumanBuilders {
   public class TestCommand {
@@ -12,7 +14,7 @@ namespace HumanBuilders {
     // Fields
     //-------------------------------------------------------------------------
     private string unityPath;
-
+    private string testResultsPath;
 
     //-------------------------------------------------------------------------
     // Constructors
@@ -24,22 +26,77 @@ namespace HumanBuilders {
     //-------------------------------------------------------------------------
     public void ParseCommandLine() {
       unityPath = CLITools.GetArgument(0);
+      testResultsPath = CLITools.GetArgument("testResults");
 
       // string command = "{0}";
     }
 
     public void Execute() {
       //  unity -batchmode -nographics -runTests -runSynchronously -projectPath
-      //  . -logFile /j/logs/test.txt -testResults /j/logs/test.xml
+      //  . -logFile -testResults /j/logs/test.xml
 
-      // unity -batchmode -nographics -executeCommand
+      // unity -batchmode -nographics -executeCommand HumanBuilders.CLI.Test
+      // -testResults /j/logs/test.xml
       
-
+      ShowResults();
     }
 
     //-------------------------------------------------------------------------
     // Helper Methods
     //-------------------------------------------------------------------------
+
+    private void ShowResults() {
+      XmlDocument doc = new XmlDocument();
+      doc.Load(testResultsPath);
+
+      foreach (XmlNode node in doc.ChildNodes) {
+        if (node.Attributes.GetNamedItem("name").ToString().Contains("test-suite")) {
+          XmlElement rootSuite = (XmlElement)node;
+          ParseTestSuite(rootSuite, 0);
+        }
+      }
+
+    }
+
+    private void ParseTestSuite(XmlElement testSuite, int depth) {
+      if (!testSuite.HasChildNodes) {
+        return;
+      }
+
+      string line = new string(' ', 3*(depth-1));
+      line += " - " + testSuite.Attributes.GetNamedItem("name").ToString() + ":";
+
+      foreach (XmlElement node in testSuite.ChildNodes) {
+        if(node.Name.Contains("test-case")) {
+          ParseTestCase(node, depth+1);
+        } else if (node.Name.Contains("test-suite")) {
+          ParseTestSuite(node, depth+1);
+        } 
+      }
+    }
+
+    private void ParseTestCase(XmlElement testCase, int depth) {
+      string line = new string(' ', 3*(depth-1));
+      line += " - " + testCase.Attributes.GetNamedItem("name").ToString() + ": ";
+      
+      string result = testCase.Attributes.GetNamedItem("result").ToString().ToLower();
+      if (result.Contains("passed")) {
+        line += "✓";
+        Console.WriteLine(line);
+      } else {
+        line += "✘";
+        Console.WriteLine(line);
+        XmlElement failure = (XmlElement)testCase.SelectSingleNode("failure");
+        XmlElement message = (XmlElement)testCase.SelectSingleNode("message");
+        XmlElement stack = (XmlElement)testCase.SelectSingleNode("stack-trace");
+        Console.Write("\n\n");
+        Console.WriteLine(message.InnerText);
+        Console.WriteLine(stack.InnerText);
+        Console.Write("\n\n");
+      }
+    }
+
+
     private List<string> GetRelevantCommands() {
       string[] args = Environment.GetCommandLineArgs();
       List<string> relevant = new List<string>();
