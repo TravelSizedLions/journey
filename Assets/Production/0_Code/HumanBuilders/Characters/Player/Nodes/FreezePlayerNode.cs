54401af9ffd01a0e52add8eda8d89c862e4ea348
Jeff Hilton <jeffhilton.code@gmail.com>
2021-05-14 20:25:02 -0600

    Adding freeze & unfreeze nodes
    + Player now stays in place when entering the final phase of the boss fight.

---
+++ Assets/Production/0_Code/HumanBuilders/Characters/Player/Nodes/FreezePlayerNode.cs
@@ -0,0 +1,32 @@
+
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace HumanBuilders {
+  
+  /// <summary>
+  /// A dialog node for disabling player movement.
+  /// </summary>
+  [NodeWidth(400)]
+  [NodeTint(NodeColors.BASIC_COLOR)]
+  [CreateNodeMenu("Player/Freeze")]
+  public class FreezePlayerNode : AutoNode { 
+
+    [Space(4, order=0)]
+    [PropertyOrder(100)]
+    [Input(connectionType=ConnectionType.Override)]
+    public EmptyConnection Input;
+
+
+    [Space(4, order=0)]
+    [PropertyOrder(100)]
+    [Output(connectionType=ConnectionType.Override)]
+    public EmptyConnection Output;
+    
+    public override void Handle(GraphEngine graphEngine) {
+      GameManager.Player.DisableCrouch(graphEngine);
+      GameManager.Player.DisableJump(graphEngine);
+      GameManager.Player.DisableMove(graphEngine);
+    }
+  }
+}
