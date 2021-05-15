591ea8b1e96ddff4f435a241e36c52b3fc97c136
Jeff Hilton <jeffhilton.code@gmail.com>
2021-05-14 20:28:59 -0600

    Adding playmode tests

---
+++ Assets/Production/3_AutomatedTesting/PlayMode/EndToEnd/CanvasOrderingTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+using HumanBuilders;
+
+namespace HumanBuilders.Tests {
+
+  public class CanvasOrderingTest {
+
+    [OneTimeSetUp]
+    public void LoadScene() {
+      SceneManager.LoadScene("crash_level_upper_deck_v2");
+    }
+
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator CanvasOrdering_CorrectWhenPaused() {
+      PauseScreen.PauseGame();
+      yield return null;
+      
+      Assert.True(PauseScreen.Canvas.sortingOrder > DialogManager.Canvas.sortingOrder);
+
+      Canvas sceneNameCanv = GameObject.Find("scene_name").transform.GetChild(0).GetComponent<Canvas>();
+      Canvas controlInputCanv = TestUtils.Find<Canvas>("control_inputs");
+
+      Assert.True(DialogManager.Canvas.sortingOrder > sceneNameCanv.sortingOrder);
+      Assert.True(sceneNameCanv.sortingOrder > controlInputCanv.sortingOrder);
+
+      yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator CanvasOrdering_CorrectWhenNotPaused() {
+      PauseScreen.ContinueGame();
+      yield return null;
+      
+      Assert.True(DialogManager.Canvas.sortingOrder > PauseScreen.Canvas.sortingOrder);
+
+      Canvas sceneNameCanv = GameObject.Find("scene_name").transform.GetChild(0).GetComponent<Canvas>();
+      Canvas controlInputCanv = TestUtils.Find<Canvas>("control_inputs");
+
+      Assert.True(DialogManager.Canvas.sortingOrder > sceneNameCanv.sortingOrder);
+      Assert.True(sceneNameCanv.sortingOrder > controlInputCanv.sortingOrder);
+
+      yield return null;
+    }
+  }
+}
---
+++ Assets/Production/3_AutomatedTesting/PlayMode/TestUtils.cs
@@ -0,0 +1,56 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HumanBuilders.Tests {
+
+  public static class TestUtils {
+
+    private static Scene ddol;
+    
+    public static T Find<T>(string name) {
+      GameObject go = Find<T>(name, typeof(T));
+      if (go != null) {
+        return go.GetComponent<T>();
+      }
+
+      return default(T);
+    }
+
+    public static GameObject Find(string name) {
+      return Find<Transform>(name, null);
+    }
+
+    public static GameObject Find<T>(string name, Type t) {
+      GameObject found = SearchRoots<T>(SceneManager.GetActiveScene().GetRootGameObjects(), name, t);
+      if (found != null) { 
+        return null;
+      }
+
+      GameObject go = new GameObject();
+      UnityEngine.GameObject.DontDestroyOnLoad(go);
+
+      found = SearchRoots<T>(go.scene.GetRootGameObjects(), name, t);
+      if (found != null) {
+        return found;
+      }
+
+      return null;
+    }
+
+    public static GameObject SearchRoots<T>(GameObject[] roots, string name, Type t) {
+      foreach (GameObject root in roots) {
+        Transform[] children = root.GetComponentsInChildren<Transform>(true);
+        foreach (Transform child in children) {
+          if ((child.name == name) && (t == null || child.GetComponent<T>() != null)) {
+            return child.gameObject;
+          }
+        }
+      }
+
+      return null;
+    }
+  }
+
+
+}
