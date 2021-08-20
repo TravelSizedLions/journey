1c44b7c65b6e490ef3abf085192ce434112f3289
Jeff Hilton <jeffhilton.code@gmail.com>
2021-08-20 01:50:57 -0600

    Adding some art for historians

--- Assets/Production/0_Code/HumanBuilders/Characters/NPCs/RandomIdleOffset.cs
+++ Assets/Production/0_Code/HumanBuilders/Characters/NPCs/RandomIdleOffset.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 namespace HumanBuilders {
   public class RandomIdleOffset : MonoBehaviour {
     private const string START_TIME = "start_time";
