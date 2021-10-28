591376e957b281a2b5edc79dedabfef112a592f0
Jeff Hilton <jeffhilton.code@gmail.com>
2021-10-28 01:13:22 -0600

    Rough draft of Gad's story line finished.

--- Assets/Production/0_Code/HumanBuilders/Subsystems/DialogSystem/AutoNodes/EndDialogNode.cs
+++ Assets/Production/0_Code/HumanBuilders/Subsystems/DialogSystem/AutoNodes/EndDialogNode.cs
@@ -1,19 +1,8 @@
-using UnityEngine;
-
 namespace HumanBuilders {
-
-  /// <summary>
-  /// A dialog node which ends a conversation.
-  /// </summary>
   [NodeTint(NodeColors.END_NODE)]
   [CreateNodeMenu("End/End Dialog")]
   public class EndDialogNode : EndNode {
-
-    public override void Handle(GraphEngine graphEngine) {
-      Debug.Log("Ending!");
-      graphEngine.EndGraph();
-    }
-
+    public override void Handle(GraphEngine graphEngine) => graphEngine.EndGraph();
     public override void PostHandle(GraphEngine graphEngine) {}
   }
 }
