b6c1d6950f2ddfe1672d1302cd3467e778168237
Jeff Hilton <jeffhilton.code@gmail.com>
2022-01-22 10:38:32 -0700

    Follows Jerrod between levels.

---
+++ Assets/Production/0_Code/HumanBuilders/Flexible/PlaceAtRoot.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace HumanBuilders {
+  public class PlaceAtRoot : MonoBehaviour {
+    private void Awake() {
+      transform.parent = null;
+    }
+  }
+
+}
