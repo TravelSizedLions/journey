df9f7cd3a62bac010b4c6fd1ab562a4188e3c853
Jeff Hilton <jeffhilton.code@gmail.com>
2023-01-17 08:43:40 -0700

    Update ordering of all canvases

--- Assets/Production/4_AutomatedTesting/PlayMode/EndToEnd/CanvasOrderingTest.cs
+++ Assets/Production/4_AutomatedTesting/PlayMode/EndToEnd/CanvasOrderingTest.cs
@@ -16,35 +16,42 @@ namespace HumanBuilders.Tests {
     }
 
     [UnityTest]
-    public IEnumerator CanvasOrdering_CorrectWhenPaused() {
-      PauseScreen.PauseGame();
-      yield return null;
-      
-      Assert.True(PauseScreen.Canvas.sortingOrder > DialogManager.Canvas.sortingOrder);
-
-      Canvas sceneNameCanv = GameObject.Find("scene_name").transform.GetChild(0).GetComponent<Canvas>();
-      Canvas controlInputCanv = TestUtils.Find<Canvas>("control_inputs");
-
-      Assert.True(DialogManager.Canvas.sortingOrder > sceneNameCanv.sortingOrder);
-      Assert.True(sceneNameCanv.sortingOrder > controlInputCanv.sortingOrder);
-
-      yield return null;
-    }
-
-    [UnityTest]
-    public IEnumerator CanvasOrdering_CorrectWhenNotPaused() {
+    public IEnumerator Canvas_Ordering_Is_Correct() {
       PauseScreen.ContinueGame();
       yield return null;
       
-      Assert.True(DialogManager.Canvas.sortingOrder > PauseScreen.Canvas.sortingOrder);
 
-      Canvas sceneNameCanv = GameObject.Find("scene_name").transform.GetChild(0).GetComponent<Canvas>();
+      Canvas sceneNameCanv = GameObject.Find("scene_name").transform.GetComponentInChildren<Canvas>();
       Canvas controlInputCanv = TestUtils.Find<Canvas>("control_inputs");
+      Canvas transitionCanv = TestUtils.Find<TransitionManager>("transition_manager").GetComponentInChildren<Canvas>();
+      Canvas walletsCanv = GameObject.Find("wallets").GetComponent<Canvas>();
 
-      Assert.True(DialogManager.Canvas.sortingOrder > sceneNameCanv.sortingOrder);
+      Assert.True(PauseScreen.Canvas.sortingOrder > DialogManager.Canvas.sortingOrder);
+      Assert.True(DialogManager.Canvas.sortingOrder > walletsCanv.sortingOrder);
+      Assert.True(walletsCanv.sortingOrder > sceneNameCanv.sortingOrder);
       Assert.True(sceneNameCanv.sortingOrder > controlInputCanv.sortingOrder);
-
+      Assert.True(controlInputCanv.sortingOrder > transitionCanv.sortingOrder);
       yield return null;
     }
+
+    // JPH NOTE: I don't know why I thought the order should be different
+    // between paused and not paused, but I don't think it effects the
+    // selectability of dialog choices, so it should be fine to keep them the
+    // same now...I think?
+    // [UnityTest]
+    // public IEnumerator CanvasOrdering_CorrectWhenNotPaused() {
+    //   PauseScreen.ContinueGame();
+    //   yield return null;
+      
+    //   Assert.True(DialogManager.Canvas.sortingOrder > PauseScreen.Canvas.sortingOrder);
+
+    //   Canvas sceneNameCanv = GameObject.Find("scene_name").transform.GetChild(0).GetComponent<Canvas>();
+    //   Canvas controlInputCanv = TestUtils.Find<Canvas>("control_inputs");
+
+    //   Assert.True(DialogManager.Canvas.sortingOrder > sceneNameCanv.sortingOrder);
+    //   Assert.True(sceneNameCanv.sortingOrder > controlInputCanv.sortingOrder);
+
+    //   yield return null;
+    // }
   }
 }
