2b605da404a19e4ac68210d441be419e1c7d9775
Jeff Hilton <jeffhilton.code@gmail.com>
2023-07-27 19:48:32 -0600

    Fix issues with skybox

--- Assets/Production/0_Code/HumanBuilders/Environment/DynamicSky.cs
+++ Assets/Production/0_Code/HumanBuilders/Environment/DynamicSky.cs
@@ -3,5 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace HumanBuilders {
-  public class DynamicSky : Singleton<DynamicSky> {}
+  public class DynamicSky : Singleton<DynamicSky> {
+
+    private void OnDestroy() {
+      var children = new List<GameObject>();
+
+      foreach (var transform in GetComponentsInChildren<Transform>(true)) {
+        children.Add(transform.gameObject);
+      }
+
+      children.ForEach(child => Destroy(child));
+    }
+  }
 }
--- Assets/Production/0_Code/HumanBuilders/Environment/Skybox.cs
+++ Assets/Production/0_Code/HumanBuilders/Environment/Skybox.cs
@@ -3,18 +3,29 @@ using UnityEngine;
 
 namespace HumanBuilders {
   public class Skybox : MonoBehaviour {
-    
+    private Camera cam;
+
     [OnInspectorGUI]
-    private void FindCamera() {
+    private Camera FindCamera() {
       Canvas canv = GetComponent<Canvas>();
       if (canv.worldCamera == null) {
         TargettingCamera cam = FindObjectOfType<TargettingCamera>();
         canv.worldCamera = cam.GetComponent<Camera>();
+        return canv.worldCamera;
       }
+
+      return null;
     }
 
     private void Awake() {
       FindCamera();
+    }
+
+    private void Update() {
+      if (cam == null) {
+        Debug.LogWarning("lost camera on skybox. searching...");
+        cam = FindCamera();
+      }
     }
   }
 }
