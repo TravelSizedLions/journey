95d9be918417f0d8a11736a73da66592377eac1e
Jeff Hilton <jeffhilton.code@gmail.com>
2023-08-13 09:52:09 -0600

    Don't create global light until after singleton deals with itself

--- Assets/Production/0_Code/HumanBuilders/Environment/GlobalLight.cs
+++ Assets/Production/0_Code/HumanBuilders/Environment/GlobalLight.cs
@@ -1,10 +1,24 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 namespace HumanBuilders {
   public class GlobalLight: Singleton<GlobalLight> {
+    public static Light2D Settings {
+      get {
+        if (Instance.settings == null) {
+          Instance.settings = Instance.gameObject.AddComponent<Light2D>();
+          Instance.settings.lightType = Light2D.LightType.Global;
+          Instance.settings.intensity = .1f;
+        }
 
-    public static Light2D Settings => (Instance.settings = Instance.settings ?? Instance.GetComponent<Light2D>());
+        return Instance.settings;
+      }
+    }
+
+    [SerializeField]
+    [ReadOnly]
+    [InfoBox("Unity throws errors if heaven forbid there's more than one global light in a scene. So, instead of having a light already in the prefab, we create a singleton and build the light only after the singleton culls other references to itself.", InfoMessageType.Warning)]
     private Light2D settings;
   }
 }
