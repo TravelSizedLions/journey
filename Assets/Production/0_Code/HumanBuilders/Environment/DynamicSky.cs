f21f4bc75105aec98376d742d19e81ae40ce4d37
Jeff Hilton <jeffhilton.code@gmail.com>
2023-08-13 08:53:23 -0600

    Address issues with global lighting on dynamic sky
    (Will also need to update execution order)

--- Assets/Production/0_Code/HumanBuilders/Environment/DynamicSky.cs
+++ Assets/Production/0_Code/HumanBuilders/Environment/DynamicSky.cs
@@ -1,9 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.Rendering.Universal;
 
 namespace HumanBuilders {
   public class DynamicSky : Singleton<DynamicSky> {
+    
+    [Range(0, 1)]
+    [Tooltip("The lighting intensity for daytime.")]
+    public float DayBrightness;
+
+    [Range(0, 1)]
+    [Tooltip("The lighting intensity for nighttime.")]
+    public float NightBrightness;
+
+    [SerializeField]
+    [ReadOnly]
+    private bool isNighttime;
+
+    public Light2D GlobalLight => (globalLight = globalLight ?? GetComponentInChildren<Light2D>());
+    private Light2D globalLight;
 
     private void OnDestroy() {
       var children = new List<GameObject>();
@@ -13,6 +29,14 @@ namespace HumanBuilders {
       }
 
       children.ForEach(child => Destroy(child));
+    }
+
+    public void SetDay() {
+      GlobalLight.intensity = DayBrightness;
+    }
+
+    public void SetNight() {
+      GlobalLight.intensity = NightBrightness;
     }
   }
 }
